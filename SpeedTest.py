9316482a6b819b2524c3cfc4d44edd0278e163ef
Lauren Shockley <laurenshockley130@icloud.com>
2023-07-24 21:15:40 -0400

    Print Update

--- SpeedTest.py
+++ SpeedTest.py
@@ -1,20 +1,20 @@
-import SpeedTest
+import SpeedTest # CLI speed test download needed to import
 
 
 test = speedtest.Speedtest()
 
 
 print("Loading a list of servers...")
-test.get_servers() # gets list of servers aval
+test.get_servers() # -> Retreving list of servers avalible
 print("Choosing the best server...")
-best = test.get_best_server() # choose best serv
-print(f"Found: {best['host']} located in {'country'}.)
-      
+best = test.get_best_server() # -> Chooses best server avalible
+print(f"Found: {best['host']} located in {'country'})
+
 print("Preforming download test...")
-download_result = test.download()
+download_result = test.download()  # -> Tests download speed of connection
 print("Preforming uplaod test...")
-upload_result = test.upload()
-ping_result = test.results.ping()
+upload_result = test.upload() # -> Tests upload speed of connection
+ping_result = test.results.ping() # -> Tests ping of connection
 
 print(f"Download speed: {download_result / 1024 / 1024:.2f} Mbit/s")
 print(f"Upload speed: {upload_result / 1024 / 1024:.2f} Mbit/s")
